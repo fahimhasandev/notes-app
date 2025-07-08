80b88376a96ab4c4b8cdb218187bd3f36a6196ff
fahimhasandev <fhasan5@wgu.edu>
2025-07-08 14:00:00 -0500

    note service set up

--- services/databseService.js
+++ services/databseService.js
@@ -1,8 +1,16 @@
 import { database } from "./appwrite";
 
 const databaseService = {
-    // List Documents
-    async listDocuments(dbId, tableID){
-
+  // List Documents
+  async listDocuments(dbId, tableID) {
+    try {
+      const response = await database.listDocuments(dbId, tableID);
+      return response.documents || [];
+    } catch (error) {
+      console.error("Error Fetchindf ocme", error.message);
+      return { error: error.message };
     }
-}
+  },
+};
+
+export default databaseService;
